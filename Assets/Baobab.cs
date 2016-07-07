ec1e593fb9d57045e6239509d46e317ba2b44696
catiejo <catiejo.pidel@gokartlabs.com>
2016-07-07 03:03:15 -0500

    adding Baobab functionality (destroy...not currently working)

---
+++ Assets/Baobab.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Baobab : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("ouch");
+	}
+	
+	// Update is called once per frame
+	void onMouseDown () {
+//		Destroy (gameObject);
+		Debug.Log("ouch");
+	}
+}
