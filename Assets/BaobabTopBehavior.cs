943f6746c6fab1d63e3ffbafcd98acf6e7e87e3c
catiejo <catiejo.pidel@gokartlabs.com>
2016-07-10 23:29:17 -0500

    all fixed.
    I had a line of code lerping the position of the baobab tree and it
    messed everything up. There were some other weird things happening,
    though, so I ended up removing and re-adding all the scripts because I
    was also accidentally ignoring some object files.

---
+++ Assets/BaobabTopBehavior.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaobabTopBehavior : MonoBehaviour {
+
+	void OnMouseDown()
+	{
+		// Destroy entire tree on click
+		Destroy (transform.parent.gameObject);
+	}
+}
---
+++ Assets/BaobabTrunkBehavior.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaobabTrunkBehavior : MonoBehaviour 
+{
+	void Start () 
+	{
+		StartCoroutine(ScaleOverTime(5));
+	}
+	void OnMouseDown()
+	{
+		Destroy (gameObject);
+	}
+
+	IEnumerator ScaleOverTime(float time)
+	{
+		float girthTime = 2f;
+		Vector3 babyScale = new Vector3(0.02f, 0.06f, 0.02f); // Cute little baobab
+		Vector3 saplingScale = new Vector3(0.15f, 0.25f, 0.15f); // Sappling baobab
+		Vector3 finalScale = new Vector3(0.25f, 0.25f, 0.25f); // Thicker, adult baobab
+
+		// Grow the whole tree
+		float currentTime = 0.0f;
+		do
+		{
+			gameObject.transform.localScale = Vector3.Lerp(babyScale, saplingScale, currentTime / time);
+			currentTime += Time.deltaTime;
+			yield return null;
+		} while (currentTime <= time);
+
+		// Reset time and grow the girth
+		currentTime = 0.0f;
+		do
+		{
+			gameObject.transform.localScale = Vector3.Lerp(saplingScale, finalScale, currentTime / girthTime);
+			currentTime += Time.deltaTime;
+			yield return null;
+		} while (currentTime <= girthTime);
+
+	}
+
+}
