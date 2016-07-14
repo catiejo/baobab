15083a8bf85d56e6f13849bacf0d1dd615c54e6f
catiejo <catiejo.pidel@gokartlabs.com>
2016-07-14 02:41:12 -0500

    ideas and musings.

--- Assets/GameControllerBehavior.cs
+++ Assets/GameControllerBehavior.cs
@@ -4,16 +4,31 @@ using System.Collections;
 public class GameControllerBehavior : MonoBehaviour {
 
 	public GUIText scoreText;
+	public PlanetBehavior planet;
 	private int score;
+	float nextPlant = 5.0F;
 
 	// Use this for initialization
 	void Start () {
-		score = -1; //Kinda hacky, but no fucks.
+//		Invoke ("planet.plantBaobab()", 0); //Start by planting a baobab.
+		score = -1;
 		updateScore ();
+//		StartCoroutine (gameDifficulty());
 	}
 
 	public void updateScore() {
 		score += 1;
 		scoreText.text = "Score: " + score;
 	}
+
+//	IEnumerator gameDifficulty () {
+//		if (score % 10 == 0) {
+//			nextPlant -= 0.5F;
+//		}
+//		yield return new WaitForSeconds (nextPlant);
+//		if (!planet.plantBaobab ()) {
+//			StopCoroutine (gameDifficulty());
+//			scoreText.text = "Game Over";
+//		}
+//	}
 }
--- Assets/PlanetBehavior.cs
+++ Assets/PlanetBehavior.cs
@@ -20,4 +20,24 @@ public class PlanetBehavior : MonoBehaviour
 		newBaobab.rotation = newBaobab.transform.rotation * Quaternion.Euler(-90, 0, 0);
 	}
 
+//	public bool plantBaobab ()
+//	{
+//		int plantAttempts = 0;
+//		Vector3 emptySpot = Random.onUnitSphere * (0.5f);
+//
+//		Transform newBaobab = Instantiate (baobab) as Transform;
+//		newBaobab.parent = gameObject.transform;
+//		while (plantAttempts < maxAttempts) {
+//			if (Physics.OverlapSphere (emptySpot, 5.0F).Length <= 1) {
+//				newBaobab.localPosition = emptySpot;
+//				// Position baobab to be facing center of planet
+//				newBaobab.LookAt(gameObject.transform.position);
+//				newBaobab.rotation = newBaobab.transform.rotation * Quaternion.Euler(-90, 0, 0); 
+//				return true;
+//			}
+//			plantAttempts++;
+//		}
+//		return false;
+//	}
+
 }
