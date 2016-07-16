5d99fe44069327e79a7375927ebd8496a1a4df29
catiejo <catiejo.pidel@gokartlabs.com>
2016-07-16 13:26:02 -0500

    reworked logic + fixed bugs for growing baobabs

--- Assets/PlanetBehavior.cs
+++ Assets/PlanetBehavior.cs
@@ -4,28 +4,29 @@ using System.Collections;
 public class PlanetBehavior : MonoBehaviour 
 {
 	public Transform baobab;
+
 	int maxAttempts = 50;
 
 	public void plantBaobab ()
 	{
 		int plantAttempts = 0;
-		Vector3 emptySpot = Random.onUnitSphere * (0.45f);
+		Vector3 emptySpot = Random.onUnitSphere * (0.5f);
 
-		Transform newBaobab = Instantiate (baobab) as Transform;
-		newBaobab.parent = gameObject.transform;
 		while (plantAttempts < maxAttempts) {
 			//This OverlapSphere isn't working...why?
-			if (Physics.OverlapSphere (emptySpot, 0.45F).Length <= 1) {
+			if (Physics.OverlapSphere (transform.TransformPoint(emptySpot), 0.15F).Length <= 1) {
+				Transform newBaobab = Instantiate (baobab) as Transform;
+				newBaobab.parent = gameObject.transform;
 				newBaobab.localPosition = emptySpot;
 				// Position baobab to be facing center of planet
 				newBaobab.LookAt(gameObject.transform.position);
 				newBaobab.rotation = newBaobab.transform.rotation * Quaternion.Euler(-90, 0, 0); 
 				return;
 			}
+			emptySpot = Random.onUnitSphere * (0.5f);
 			plantAttempts++;
 		}
 		Debug.Log ("failed to plant baobab");
-		Destroy (newBaobab.gameObject);
 	}
 
 }
